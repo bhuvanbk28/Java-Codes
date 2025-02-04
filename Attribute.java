04f7f8c98538741c7b11f7d25fa972f1a6e5b841
bhuvanbk28 <137741961+bhuvanbk28@users.noreply.github.com>
2025-02-04 10:36:04 +0530

    Add files via upload

---
+++ Attribute.java
@@ -0,0 +1,8 @@
+//public class Attribute {
+//    final int x = 10;
+//    public static void main(String[] args) {
+//        Attribute myObj = new Attribute();
+//        myObj.x=25; // will generate an error: cannot assign a value to a final variable
+//        System.out.println(myObj.x);
+//    }
+//}
---
+++ Student.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Student {
+    public static void main(String[]args){
+        int c,d,e;
+        String a,b;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        a=s.next();
+        System.out.println("Enter Last Name: ");
+        b=s.next();
+        System.out.println("Enter Id: ");
+        c=s.nextInt();
+        System.out.println("Enter Mark1: ");
+        d=s.nextInt();
+        System.out.println("Enter Mark2: ");
+        e=s.nextInt();
+        System.out.println("Student Name is "+(a+b));
+        System.out.println("Student Id is"+c);
+        System.out.println("Average is "+(d+e)/2);
+
+    }
+}
